18287f14cb3979b6771889b5cd664dd709fcefe8
johnatanpalmer <162459108+johnatanpalmer@users.noreply.github.com>
2025-08-05 15:16:25 -0300

    Create SERVICESREPUTATIONNFT_RWA.sol

---
+++ contracts/SERVICESREPUTATIONNFT_RWA.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/access/Ownable.sol";
+
+/**
+ * @title ServicesReputationNFT
+ * @notice An ERC721 contract to issue achievement NFTs to high-performing service providers.
+ * Ownership of this contract will be transferred to the DynamicReputationToken contract.
+ */
+contract ServicesReputationNFT is ERC721, Ownable {
+    uint256 private _nextTokenId;
+    
+    // Mapping from the token ID to its "vintage" (year and month of the achievement)
+    mapping(uint256 => string) public tokenVintage;
+
+    constructor(address initialOwner)
+        ERC721("E-co.lab Services Achiever", "E-ACH")
+        Ownable(initialOwner)
+    {}
+
+    /**
+     * @notice Only the owner (the reputation contract) can mint a new achievement NFT.
+     * @param to The address of the service provider who will receive the NFT.
+     * @param vintage A string representing the month and year of the achievement (e.g., "2024-08").
+     */
+    function awardAchievement(address to, string memory vintage) external onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        tokenVintage[tokenId] = vintage;
+        _safeMint(to, tokenId);
+    }
+}
